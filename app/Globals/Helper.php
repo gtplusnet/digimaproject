010ed189d92e51102a77d1f44d24e36767bca68d
Arcylen Gutierrez <arcylen103095@gmail.com>
2018-03-02 12:02:35 +0800

    change

--- app/Globals/Helper.php
+++ app/Globals/Helper.php
@@ -26,34 +26,71 @@ class Helper
             return false;
         }
     }
-    public static function onlineAgo($datetime, $full = false)
+    public static function onlineAgo($time_ago, $full = false)
     {
-        $now = new DateTime;
-        $ago = new DateTime($datetime);
-        $diff = $now->diff($ago);
-
-        $diff->w = floor($diff->d / 7);
-        $diff->d -= $diff->w * 7;
-
-        $string = array(
-            'y' => 'year',
-            'm' => 'month',
-            'w' => 'week',
-            'd' => 'day',
-            'h' => 'hour',
-            'i' => 'minute',
-            's' => 'second',
-        );
-        foreach ($string as $k => &$v) {
-            if ($diff->$k) {
-                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-            } else {
-                unset($string[$k]);
+        $time_ago = strtotime($time_ago);
+        $cur_time   = time();
+        $time_elapsed   = $cur_time - $time_ago;
+        $seconds    = $time_elapsed ;
+        $minutes    = round($time_elapsed / 60 );
+        $hours      = round($time_elapsed / 3600);
+        $days       = round($time_elapsed / 86400 );
+        $weeks      = round($time_elapsed / 604800);
+        $months     = round($time_elapsed / 2600640 );
+        $years      = round($time_elapsed / 31207680 );
+        // Seconds
+        if($seconds <= 60){
+            return "<span style='color: green;'>Online</span>";
+        }
+        //Minutes
+        else if($minutes <=60){
+            if($minutes==1){
+                return "one minute ago";
+            }
+            else{
+                return "$minutes minutes ago";
             }
         }
-
-        if (!$full) $string = array_slice($string, 0, 1);
-        return $string ? implode(', ', $string) . ' ago' : '<span style="color: green;">Online</span>';
+        //Hours
+        else if($hours <=24){
+            if($hours==1){
+                return "an hour ago";
+            }else{
+                return "$hours hrs ago";
+            }
+        }
+        //Days
+        else if($days <= 7){
+            if($days==1){
+                return "yesterday";
+            }else{
+                return "$days days ago";
+            }
+        }
+        //Weeks
+        else if($weeks <= 4.3){
+            if($weeks==1){
+                return "a week ago";
+            }else{
+                return "$weeks weeks ago";
+            }
+        }
+        //Months
+        else if($months <=12){
+            if($months==1){
+                return "a month ago";
+            }else{
+                return "$months months ago";
+            }
+        }
+        //Years
+        else{
+            if($years==1){
+                return "one year ago";
+            }else{
+                return "$years years ago";
+            }
+        }
     }
     public static function timeUntil($deadline)
     {
